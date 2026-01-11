7947f51c681b86d249c9f4fb44d63e110c528c3c
beep <git@beeps.email>
2026-01-11 22:09:31 +0100

    fix builds maybe

--- docs/sidebars.ts
+++ docs/sidebars.ts
@@ -14,7 +14,7 @@ import type {SidebarsConfig} from '@docusaurus/plugin-content-docs';
  */
 const sidebars: SidebarsConfig = {
   tutorialSidebar: [
-    'Installing',
+    'index',
     'Troubleshooting',
     'Configuration',
     {
